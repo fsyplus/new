b18ba4a918f79bed5937d6a947cd4bb108abc78c
fsyplus <1277235885@qq.com>
2023-09-21 09:45:04 +0800

    feat:first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>数据{{ name }}</div>
+    <button class="haha" @click="back"></button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import router from '@/router';
+
+export default {
+  name: 'HomeView',
+  components: { 
+    HelloWorld 
+  },
+  data(){
+    return {
+      name:'送达'
+    }
+  },
+  methods:{
+    back(){
+      console.log('上班第一天');
+    }
+  }
+}
+</script>
+<style>
+.haha{
+  width: 200px;
+  height: 200px;
+  background-color: red;
+}
+</style>
